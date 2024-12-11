0b3a7b6d192d25263f19b3d1996fb875a33fd653
craig goble <c_goble@u.pacific.edu>
2024-12-11 09:04:25 -0500

    day9

--- day8/program.py
+++ day8/program.py
@@ -19,8 +19,39 @@ def process1():
                 
     return len([0 for r,c in antinode  if 0 <= r < rows and 0 <= c < cols])
     #print(len([0 for r,c in antinodes if 0 <= r < rows and 0 <= c < cols]))
+def process2():
+    grid = [line.strip() for line in open("input.txt")]
+
+    rows = len(grid)
+    cols = len(grid[0])
+
+    antennas = {}
+
+    for r, row in enumerate(grid):
+        for c, char in enumerate(row):
+            if char != ".":
+                if char not in antennas: antennas[char] = []
+                antennas[char].append((r, c))
+
+    antinodes = set()
+
+    for array in antennas.values():
+        for i in range(len(array)):
+            for j in range(len(array)):
+                if i == j: continue
+                r1, c1 = array[i]
+                r2, c2 = array[j]
+                dr = r2 - r1
+                dc = c2 - c1
+                r = r1
+                c = c1
+                while 0 <= r < rows and 0 <= c < cols:
+                    antinodes.add((r, c))
+                    r += dr
+                    c += dc
+    return len(antinodes)
 if __name__ == "__main__":
-    print(process1())
+    print(process2())
 
 
 
